14b674eafef761acefb9b6c17d4a3e24ce788419
minju7346 <minju7346@naver.com>
2023-07-31 17:41:32 +0900

    error

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
-const mysql = require("mysql")
+const mysql = require("mysql");
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST ,
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
